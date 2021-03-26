e5023bc5d5d8c8b20f7e89e9a99724577e2d23ee
Sergey Kuligin <formyjobonlynow@gmail.com>
2021-03-26 21:20:16 +0500

    2120-032621

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e9385238ca9317a0cdb6352839a84ee5.js"
+  "/precache-manifest.dee4922f8ab0192fa1b29938035e2033.js"
 );
 
 self.addEventListener('message', (event) => {
