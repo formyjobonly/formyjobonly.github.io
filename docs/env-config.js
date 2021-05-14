f32465fbce8c0c3ae68a49b1ad1e1a804980cb88
1-ACER <formyjobonlynow@gmail.com>
2021-05-14 13:29:34 +0500

    1329-051421

--- docs/env-config.js
+++ docs/env-config.js
@@ -1,5 +1,4 @@
 window.__preorder_env__ = {
-  REACT_APP_API_HOST: "https://cdc68e93c016.ngrok.io/",
-  REACT_EDITOR: "code",
+  REACT_APP_API_HOST: "https://075064280d45.ngrok.io/",
   BROWSER: "none",
 }
